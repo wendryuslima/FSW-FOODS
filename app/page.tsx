70c3da3a9e77dce534ed43af152aca3c3e359e9f
wendryuslima <141366401+wendryuslima@users.noreply.github.com>
2024-07-07 18:09:11 -0300

    feat: add category item and list

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,20 @@
+import CategoryList from "./_components/category-list";
 import Header from "./_components/header";
+import Search from "./_components/search";
 
 const Home = () => {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <div className="px-5 pt-6">
+        <Search />
+      </div>
+
+      <div className="p-x5 pt-6">
+        <CategoryList />
+      </div>
+    </>
+  );
 };
 
 export default Home;
