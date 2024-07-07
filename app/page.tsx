d1bd7f309a6dd7b3b588cfa28626c24281514970
wendryuslima <141366401+wendryuslima@users.noreply.github.com>
2024-07-07 18:20:30 -0300

    feat: add banner homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import CategoryList from "./_components/category-list";
 import Header from "./_components/header";
 import Search from "./_components/search";
@@ -13,6 +14,16 @@ const Home = () => {
       <div className="p-x5 pt-6">
         <CategoryList />
       </div>
+
+      <Image
+        src={"/promo-banner-01.png"}
+        alt="Promo-banner"
+        height={0}
+        width={0}
+        className="h-auto w-full p-3"
+        sizes="100vw"
+        quality={100}
+      />
     </>
   );
 };
