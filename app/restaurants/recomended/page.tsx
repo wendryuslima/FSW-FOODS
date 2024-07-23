68448eedf195e61fc288002c879fcf3233d2a922
wendryuslima <141366401+wendryuslima@users.noreply.github.com>
2024-07-23 16:06:31 -0300

    style: alteration quality for logo

--- app/restaurants/recomended/page.tsx
+++ app/restaurants/recomended/page.tsx
@@ -8,6 +8,7 @@ const RecomendedRestaurants = async () => {
   return (
     <>
       <Header />
+
       <div className="px-5 py-6">
         <h2 className="mb-6  text-lg font-semibold">
           Restaurantes recomendados
