993989891a566ff276ea9d3006527b4712cb0628
wendryuslima <141366401+wendryuslima@users.noreply.github.com>
2024-08-19 20:50:03 -0300

    feat: ensure restaurant can be favorited

--- app/_components/restaurant-item.tsx
+++ app/_components/restaurant-item.tsx
@@ -1,17 +1,45 @@
-import { Restaurant } from "@prisma/client";
-import { BikeIcon, StarIcon, TimerIcon } from "lucide-react";
+"use client";
+
+import { Restaurant, UserFavoriteRestaurant } from "@prisma/client";
+import { BikeIcon, HeartIcon, StarIcon, TimerIcon } from "lucide-react";
 import Image from "next/image";
 import { formatCurrency } from "../_helpers/price";
-
+import { toggleFavoriteRestaurant } from "../_action/restaurant";
 import Link from "next/link";
 import { cn } from "../_lib/utils";
+import { Button } from "./ui/button";
+import { toast } from "sonner";
 
 interface RestaurantItemProps {
+  userId?: string;
   restaurant: Restaurant;
   className?: string;
+  userFavoritesRestaurants: UserFavoriteRestaurant[];
 }
 
-const RestaurantItem = ({ restaurant, className }: RestaurantItemProps) => {
+const RestaurantItem = ({
+  restaurant,
+  className,
+  userId,
+  userFavoritesRestaurants,
+}: RestaurantItemProps) => {
+  const isFavorite = userFavoritesRestaurants.some(
+    (fav) => fav.restaurantId === restaurant.id,
+  );
+  const handleFavoriteClick = async () => {
+    if (!userId) return;
+
+    try {
+      await toggleFavoriteRestaurant(userId, restaurant.id);
+      toast.success(
+        isFavorite
+          ? "Restauranter removido dos favoritos"
+          : "Restaurante adicionado aos favoritos",
+      );
+    } catch (error) {
+      toast.error("Erro ao favoritar restaurante");
+    }
+  };
   return (
     <Link
       className={cn("min-w-[266px] max-w-[266px]", className)}
@@ -30,7 +58,18 @@ const RestaurantItem = ({ restaurant, className }: RestaurantItemProps) => {
             <StarIcon className="fill-yellow-500 text-yellow-500" size={12} />
             <span className=" text-xs font-semibold">5.0</span>
           </div>
+
+          {userId && (
+            <Button
+              size="icon"
+              className="absolute right-2 top-2 h-7 w-7 rounded-full bg-gray-700"
+              onClick={handleFavoriteClick}
+            >
+              <HeartIcon size={16} className="fill-white"></HeartIcon>
+            </Button>
+          )}
         </div>
+
         <div>
           <h3 className="text-sm font-semibold">{restaurant.name}</h3>
 
--- app/_components/restaurant-list.tsx
+++ app/_components/restaurant-list.tsx
@@ -1,12 +1,25 @@
+import { getServerSession } from "next-auth";
 import { db } from "../_lib/prisma";
 import RestaurantItem from "./restaurant-item";
+import { authOptions } from "../_lib/auth";
 
 const RestaurantList = async () => {
+  const session = await getServerSession(authOptions);
+
   const restaurants = await db.restaurant.findMany({ take: 10 });
+  const userFavoritesRestaurants = await db.userFavoriteRestaurant.findMany({
+    where: { userId: session?.user?.id },
+  });
+
   return (
     <div className="flex gap-4 overflow-x-scroll [&::-webkit-scrollbar]:hidden">
       {restaurants.map((restaurants) => (
-        <RestaurantItem restaurant={restaurants} key={restaurants.id} />
+        <RestaurantItem
+          restaurant={restaurants}
+          key={restaurants.id}
+          userId={session?.user?.id}
+          userFavoritesRestaurants={userFavoritesRestaurants}
+        />
       ))}
     </div>
   );
